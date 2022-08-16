6733c42eb94b9debe4e4223c7cc42baff32cac16
fafaelbernardo@gmail.com <fafaelbernardo.com.br>
2022-08-16 11:34:12 -0300

    inclusao da mensagem git hub

--- projeto.git/src/main/java/projeto/git/HelloWorld.java
+++ projeto.git/src/main/java/projeto/git/HelloWorld.java
@@ -1,9 +1,10 @@
 package projeto.git;
 
-public class HelloWord {
+public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Word");
+		System.out.println("Hello World");
+		System.out.println("Git hub");
 	}
 
 }
